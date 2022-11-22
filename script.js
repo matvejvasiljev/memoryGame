fcbc39477b946951573516eba9d9698072feb3a0
matvejvasiljev <matvej.vasiljev.2010@icloud.com>
2022-11-22 21:30:15 +0300

    Made a win animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let winningText = document.getElementById("winningText")
+let balloons = document.getElementById("balloons")
 let attemptCount = document.getElementById("attempts")
 let game = document.getElementById("game")
 
@@ -9,6 +10,27 @@ let points = 0
 
 let images = []
 let opened = []
+function winAnim() {
+    for (let i = 0; i < 20; i++) {
+        let balloon = document.createElement("img")
+        balloon.classList.add("balloon")
+        balloon.src = "balloon.svg"
+        balloons.appendChild(balloon)
+
+        let animTime = Math.random() * 3 + 3
+
+        balloon.style.left = Math.random() * window.innerWidth - 100 + "px"
+        balloon.style.animation = "rise " + animTime + "s ease-in 1 normal"
+        setTimeout(() => {
+            balloon.style.display = "none"
+        }, animTime * 1000 - 100);
+        balloon.style.filter = "hue-rotate(" + Math.floor(Math.random() * 360) + "deg)"
+        balloon.style.transform = "scale(" + Math.floor((Math.random() / 2 + 0.5) * 100) / 100 + ")"
+    }
+}
+
+
+
 
 for (let i = 0; i < 12; i++) {
     images.push("card" + i + ".jpg")
@@ -36,26 +58,51 @@ for (let i = 0; i < 24; i++) {
         console.log(images[i]);
         img.src = "./cards/" + images[i]
         opened.push(img)
+
+        img.style.pointerEvents = "none"
+
         if (opened.length > 1) {
+            let cards = document.getElementsByTagName("img")
             attempts += 1
             attemptCount.innerHTML = "Attempts: " + attempts
+
             if (opened[0].src == opened[1].src) {
                 console.log("correct");
+                opened[0].style.transition = "1s"
+                opened[1].style.transition = "1s"
+                opened[0].style.transform = "scale(0.9)"
+                opened[1].style.transform = "scale(0.9)"
+                opened[0].style.filter = "saturate(0.6)"
+                opened[1].style.filter = "saturate(0.6)"
+                opened[0].style.pointerEvents = "none"
+                opened[1].style.pointerEvents = "none"
                 points += 1
                 opened = []
+
                 if (points > 11) {
                     winningText.style.transform = "scale(1)"
-                    setTimeout(function(){
+                    for (let card of cards) {
+                        card.style.transform = "scale(1)"
+                        card.style.filter = "saturate(1)"
+                    }
+                    setTimeout(function () {
                         winningText.style.animation = "glow 3s ease-in-out infinite alternate"
                     }, 1000)
+                    winAnim()
                 }
             }
             else {
                 console.log("incorrect");
+                for (let card of cards) {
+                    card.style.pointerEvents = "none"
+                }
                 setTimeout(() => {
                     opened[0].src = cardClosed
                     opened[1].src = cardClosed
                     opened = []
+                    for (let card of cards) {
+                        card.style.pointerEvents = "auto"
+                    }
                 }, 1000);
             }
         }
