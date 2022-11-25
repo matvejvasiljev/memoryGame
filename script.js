ac695e599d6c309a09a6fb711ee1e636fb2044c6
matvejvasiljev <matvej.vasiljev.2010@icloud.com>
2022-11-25 15:05:31 +0300

    updating win animation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let winningText = document.getElementById("winningText")
 let balloons = document.getElementById("balloons")
 let attemptCount = document.getElementById("attempts")
 let game = document.getElementById("game")
+let restart = document.getElementById("restart")
 
 let cardClosed = "./cards/cardClosed.jpg"
 
@@ -10,26 +11,43 @@ let points = 0
 
 let images = []
 let opened = []
-function winAnim() {
-    for (let i = 0; i < 20; i++) {
-        let balloon = document.createElement("img")
-        balloon.classList.add("balloon")
-        balloon.src = "balloon.svg"
-        balloons.appendChild(balloon)
+for (let i = 0; i < 20; i++) {
+    let balloon = document.createElement("img")
+    balloon.classList.add("balloon")
+    balloon.src = "balloon.svg"
+    balloons.appendChild(balloon)
 
+    balloon.style.filter = "hue-rotate(" + Math.floor(Math.random() * 360) + "deg)"
+    balloon.setAttribute("data-scale", Math.floor((Math.random() / 2 + 0.5) * 100) / 100)
+}
+function winAnim() {
+    for (let balloon of balloons.children) {
         let animTime = Math.random() * 3 + 3
+        balloon.style.transition = animTime + "s"
+        console.log("win animation");
 
         balloon.style.left = Math.random() * window.innerWidth - 100 + "px"
-        balloon.style.top = "0"
-        balloon.style.transform = "translateY(-" + (window.innerHeight + 550) + "px) " + "scale(" + Math.floor((Math.random() / 2 + 0.5) * 100) / 100 + ")"
+        balloon.style.transform = "translateY(-" + (window.innerHeight + 550) + "px) " + "scale(" + balloon.dataset.scale + ")"
         // balloon.style.animation = "rise " + animTime + "s ease-in 1 normal"
         // setTimeout(() => {
         //     balloon.style.display = "none"
         // }, animTime * 1000 - 100);
-        balloon.style.filter = "hue-rotate(" + Math.floor(Math.random() * 360) + "deg)"
     }
 }
 
+restart.onclick = function () {
+    opened = []
+    for (let card of game.children) {
+        card.src = cardClosed
+        card.style.pointerEvents = "auto"
+        card.style.transition = "0.1s"
+        card.style.transform = "scale(1)"
+        card.style.filter = "saturate(1)"
+
+        attempts = 0
+        attemptCount.innerHTML = "Attempts: " + attempts
+    }
+}
 
 
 for (let i = 0; i < 12; i++) {
